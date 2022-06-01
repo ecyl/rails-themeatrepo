036632da5b13ac3fe7e067cda925437009c54d6a
raymond ong <rayjosong@gmail.com>
2022-06-01 15:10:40 +0800

    create new and show for butcher ctrl

--- app/controllers/butcheries_controller.rb
+++ app/controllers/butcheries_controller.rb
@@ -1,12 +1,21 @@
 class ButcheriesController < ApplicationController
+  before_action :find_butcher, only: :show
   # create butchery
   # view all possible bucheries
   # view information about a butchery
 
   def new
+    @butcher = Butcher.new
   end
 
   def create
+    @butcher = Butcher.new(butcher_params)
+
+    if @butcher.save
+      redirect_to user_butcheries_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -14,4 +23,15 @@ class ButcheriesController < ApplicationController
 
   private
 
+  def find_butcher
+    @butcher = Butcher.find(params[:id])
+  end
+
+  def find_user
+
+  end
+
+  def butcher_params
+    params.require(:butcher).permit(:name, :address, :phone_number)
+  end
 end
