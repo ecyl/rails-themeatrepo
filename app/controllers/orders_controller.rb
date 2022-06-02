f2b0d264bd72cddbbe53ae512ee026f314f24702
raymond ong <raymondong.nus@gmail.com>
2022-06-02 22:41:29 +0800

    solved orders from butchery page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   # find the user
   before_action :find_product_id, only: [:show, :create, :new]
-  before_action :find_user, only: [:show, :new, :create]
+  # before_action :find_user, only: [:show, :new, :create]
 
   # view all orders
   def index
@@ -17,11 +17,11 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     # assign order to user
-    @order.user = @user
+    @order.user = current_user
     @order.product = @product
 
     if @order.save
-      redirect_to user_product_orders_path(@user, @product)
+      redirect_to product_orders_path(@product)
     else
       render :new
     end
@@ -36,9 +36,9 @@ class OrdersController < ApplicationController
     @product = Product.find(params[:product_id])
   end
 
-  def find_user
-    @user = User.find(params[:user_id])
-  end
+  # def find_user
+  #   @user = User.find(params[:user_id])
+  # end
 
   def order_params
     params.require(:order).permit(:quantity)
