0c1590105945ad281efd19d03d56c64ff1ecd7f5
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-02 15:19:40 +0800

    seller add product, view inventory

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,26 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_user_role
   def home
+    @role = current_user.role
+    @user_id = 11
+    @user = User.find(@user_id)
+
+    @butcheries = @user.butcheries if @role == 'seller'
+
+    # include orders when done
   end
+
+  # def show
+
+  # end
+
+
+
+  private
+
+  def set_user_role
+    @role = current_user.role
+  end
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
+  # before_action :set_user
+  before_action :set_butchery, :set_user
   def index
-    @products = Product.all
+    @products = @butchery.products
+    @butcher = Butchery.find(params[:butchery_id])
   end
 
   def show
@@ -14,9 +17,9 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-
+    @product.butchery = @butchery
     if @product.save
-      redirect_to product_path(@product), notice: 'Product was successfully created'
+      redirect_to user_butchery_products_path(@user, @butchery), notice: 'Product was successfully created'
     else
       render :new
     end
@@ -32,6 +35,14 @@ class ProductsController < ApplicationController
   end
 
   private
+
+  def set_butchery
+    @butchery = Butchery.find(params[:butchery_id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :price, :country, :expiration_date)
