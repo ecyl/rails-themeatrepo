72f7a404df6bfadee0415338e7d755fc8f1e2750
helen tan <helentanmz0909@gmail.com>
2022-06-04 23:49:05 +0800

    commented out console.log

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -3,7 +3,7 @@ import Typed from "typed.js"
 
 export default class extends Controller {
   connect() {
-    console.log("In the typed_js controller");
+    // console.log("In the typed_js controller");
     new Typed(this.element, {
       strings: ["meats", "beef", "pork", "poultry", "lamb"],
       typeSpeed: 70,
