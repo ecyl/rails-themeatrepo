b6949e5d75ee4fa3e0bcbc0a5aa716df01d4b2f4
Germaine Wong <germainewong0@gmail.com>
2022-06-05 13:29:22 +0800

    fixed navbar, logout, edited order controller and index display, and added a home page for the seller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'show', to: 'pages#show'
+
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :butcheries, only: [:index, :show, :new, :create] do
     resources :products, only: [:new, :create]
