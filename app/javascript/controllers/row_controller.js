33744d2ac6c24b1f01aa71d4db60359350194895
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-06 14:51:44 +0800

    made entire dashboard row clickable with right path

--- app/javascript/controllers/row_controller.js
+++ app/javascript/controllers/row_controller.js
@@ -1,12 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["row"]
 
   connect() {
     console.log("Row controller is now connected!")
   };
 
-  sendToLink() {
+  sendToLink(event) {
+    // event.preventDefault();
+    const row = event.currentTarget;
+    const butchery_id = row.dataset['butcheryId']
+    window.location.href = `butcheries/${butchery_id}`
+  }
 
+  sendToAdd(event) {
+    const row = event.currentTarget;
+    window.location.href = `butcheries/new`
   }
 }
